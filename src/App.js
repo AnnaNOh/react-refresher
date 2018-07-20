06f4a07efd783c5fa52c4958fe7dd966d5d83caa
Anna <annaoh1994@gmail.com>
2018-07-19 17:06:29 -0700

    Finished up to Section 3 Lecture 45 of refresher course.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.jsx';
+
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age: 26}
+    ],
+    button: "unclicked"
+  }
+
+  switchNameHandler = () => {
+    // console.log("Was clicked")
+    if (this.state.button === "unclicked") {
+      this.setState({button: "clicked"});
+    } else {
+      this.setState({button: "unclicked"});
+    }
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: "Max", age: 28},
+        {name: event.target.value, age: 29},
+        {name: "Stephanie", age: 26}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <h1> Hi, this is the App </h1>
+        <button onClick={() => this.switchNameHandler()}> click this </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}> Hi there. </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+    </div>
     );
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, "Hi, this is the App"));
   }
 }
 
