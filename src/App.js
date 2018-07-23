bd9d837d399d331f756bd4648e5d43c61c5a5731
Anna <annaoh1994@gmail.com>
2018-07-23 15:36:48 -0700

    S5L66 removed radium

--- src/App.js
+++ src/App.js
@@ -2,66 +2,93 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.jsx';
 
-
 class App extends Component {
   state = {
     persons: [
-      {name: "Max", age: 28},
-      {name: "Manu", age: 29},
-      {name: "Stephanie", age: 26}
+      {id: 'asfa1', name: "Max", age: 28},
+      {id: 'vasdf1', name: "Manu", age: 29},
+      {id: 'asdf11', name: "Stephanie", age: 26}
     ],
-    button: "unclicked"
+    button: "unclicked",
+    showPersons: false
   }
 
-  switchNameHandler = () => {
-    // console.log("Was clicked")
-    if (this.state.button === "unclicked") {
-      this.setState({button: "clicked"});
-    } else {
-      this.setState({button: "unclicked"});
-    }
+  deletePersonHandler = (personIndex) => {
+    // should not change original state. should make changes on a copy and then repoint;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]; // modern array copying of array
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: "Max", age: 28},
-        {name: event.target.value, age: 29},
-        {name: "Stephanie", age: 26}
-      ]
-    })
-  }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
 
-  render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+    const person = {
+      ...this.state.persons[personIndex]
     };
+    // const person = Object.assign({}, this.state.persons[personIndex]); does the same thing but in ES5
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
+  }
+
+  togglePersonsHandler = () => {
+  // this sets up rather than togglerPersonsHandler() = ensures this can be used
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  // React can't have if statement in its return but you can use turnary
+  // Should put the conditional in the render instead of return
+  render() {
+
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+        })}
+        </div>
+      );
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold');  // classes = ['red', 'bold']
+    }
 
     return (
-      <div className="App">
-        <h1> Hi, this is the App </h1>
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler()}> click this </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler}> Hi there. </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
-    </div>
+
+        <div className="App">
+          <h1> Hi, this is the App </h1>
+          <p className={classes.join(' ')}> This is really working! </p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}> click this
+          </button>
+          {persons}
+
+        </div>
+
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, "Hi, this is the App"));
   }
 }
 
+// higher order component
 export default App;
